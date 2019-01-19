f4294c2be1959977a436211271b0d7f89d44c760
nokdoot <nokdoot@naver.com>
2019-01-19 17:04:42 +0900

    DBIx

--- test.pl
+++ test.pl
@@ -1,22 +1,8 @@
 #!/usr/bin/env perl
 
-use strict;
-use App::Pod2CpanHtml;
+my $str = `git diff --name-status master`;
 
-
-my $parser = App::Pod2CpanHtml->new();
-
-if (defined $ARGV[0]) {
-    open IN, $ARGV[0]  or die "Couldn't open $ARGV[0]: $!\n";
-} else {
-    *IN = *STDIN;
+foreach my $line (split /[\r\n]+/, $str)
+{
+	print $line."\n\n";
 }
-
-if (defined $ARGV[1]) {
-    open OUT, ">$ARGV[1]" or die "Couldn't open $ARGV[1]: $!\n";
-} else {
-    *OUT = *STDOUT;
-}
-
-$parser->output_fh(*OUT);
-$parser->parse_file(*IN);
