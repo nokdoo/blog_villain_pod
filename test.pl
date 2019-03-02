201aa2474e66fac08f6d1cca1b0b9749f3a28349
nokdoot <nokdoot@naver.com>
2019-03-02 09:29:30 +0900

    test

--- test.pl
+++ test.pl
@@ -1,8 +1,26 @@
 #!/usr/bin/env perl
 
-my $str = `git diff --name-status master`;
+use strict;
+use warnings;
+use lib './';
+use Term::ANSIColor;
+use Data::Dumper;
 
-foreach my $line (split /[\r\n]+/, $str)
-{
-	print $line."\n\n";
-}
+use HTML::TreeBuilder;
+
+# Turn HTML into DOM tree
+my $html_string = do {
+	open (my $file, '<:encoding(utf8)', './ttttt') or die "T.T";
+	local $/;
+	<$file>;
+};
+
+my $root = HTML::TreeBuilder->new_from_content($html_string);
+
+print $root->{_body}->{_content};
+
+
+
+# open ( my $file, '>', 'tttttt' ) or die;
+# print $file $root->{_body}->as_HTML;
+
